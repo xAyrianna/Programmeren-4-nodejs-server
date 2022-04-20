e578bb2469e9ad0b5cb311c4ccd48402e9582169
Ayrianna <aechatlein@gmail.com>
2022-04-20 16:26:55 +0200

    Added movie get and post requests

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
-const http = require("http");
-
+const express = require("express");
+const req = require("express/lib/request");
+const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
 
-const result = {
-  code: 200,
-  message: "Hello World",
-};
+app.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(JSON.stringify(result));
+let database = [];
+let id = 0;
+
+app.all("/", (req, res, next) => {
+  const method = req.method;
+  console.log(`Methode ${method} aangeroepen`);
+  next();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: "Hello World!",
+  });
+});
+
+app.post("/api/movie", (req, res) => {
+  let movie = req.body;
+  console.log(movie);
+  id++;
+  movie = {
+    id,
+    ...movie,
+  };
+
+  database.push(movie);
+  console.log(database);
+  res.status(201).json({
+    status: 201,
+    result: movie,
+  });
+});
+
+app.get("/api/movie", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: database,
+  });
+});
+
+app.get("/api/movie/:movieId", (req, res) => {
+  const movieId = req.params.movieId;
+  let movie = database.filter((item) => item.id == movieId);
+  if (movie.length > 0) {
+    console.log(movie);
+    res.status(200).json({
+      status: 200,
+      result: movie,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `Movie with ID ${movieId} not found`,
+    });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: 400,
+    result: "End-point not found",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });

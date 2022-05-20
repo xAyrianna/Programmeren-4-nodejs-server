6be678373da04a7cfe7581315e756a167be1730d
Ayrianna <aechatlein@gmail.com>
2022-05-20 16:14:10 +0200

    Changed format

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ const loglevel = (module.exports = {
   jwtSecretKey: process.env.JWT_SECRET,
 
   logger: require("tracer").console({
-    format: ["{{timestamp}} [{{title}}] {{file}}:{{line}} : {{message}}"],
+    format: ["{{timestamp}} [{{title}}] {{file}}:{{line}}: {{message}}"],
     preprocess: function (data) {
       data.title = data.title.toUpperCase();
     },

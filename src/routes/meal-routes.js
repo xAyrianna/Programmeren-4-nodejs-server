d9df01fcee1aa5b8857b610ac2c94462b561d297
Ayrianna <aechatlein@gmail.com>
2022-05-22 15:42:43 +0200

    Changed order

--- src/routes/meal-routes.js
+++ src/routes/meal-routes.js
@@ -11,19 +11,19 @@ router.post(
   mealController.addMeal
 );
 
-//UC-302 Get all meals
-router.get("/meal", mealController.getAllMeals);
-
-//UC-303 Get a single meal by id
-router.get("/meal/:mealId", mealController.getMealbyId);
-
-//UC-304 Update a single meal
+//UC-302 Update a single meal
 router.put(
   "/meal/:mealId",
   authController.validateToken,
   mealController.validateUpdateMeal,
   mealController.updateMealById
 );
+
+//UC-303 Get List of meals
+router.get("/meal", mealController.getAllMeals);
+
+//UC-304 Get a single meal by id
+router.get("/meal/:mealId", mealController.getMealbyId);
 
 //UC-305 Delete a meal
 router.delete(
